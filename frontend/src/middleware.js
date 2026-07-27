181f8a052cff22cc350e99f64883ae353ffc1c70
zkr hz <zakariaxhizzi@gmail.com>
2026-07-27 10:59:51 +0100

    Update middleware.js 5

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -14,7 +14,7 @@ const publicRoutes = new Set([
 export async function middleware(request) {
   const { pathname } = request.nextUrl;
 
- if (pathname.startsWith("/api/") {
+if (pathname.startsWith("/api/") || pathname.startsWith("/auth/callback")) {
   return NextResponse.next();
 }
 
