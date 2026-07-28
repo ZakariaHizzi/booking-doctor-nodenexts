0051d779b887c77735d3d963de3824f92aac2c94
zkr hz <zakariaxhizzi@gmail.com>
2026-07-28 21:05:07 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const connectDB = async () => {
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
+    origin: ["https://fullstack-doctor-appointment.vercel.app"],
     credentials: true,
   }),
 );
