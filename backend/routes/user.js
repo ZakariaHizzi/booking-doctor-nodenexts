80e3192d9e3bd1c59c5b0c316622471457ba6f88
zkr hz <zakariaxhizzi@gmail.com>
2026-07-28 17:45:57 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -10,6 +10,8 @@ const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV,
   sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: "/",
 };
 
 function setAuthCookie(res, token) {
