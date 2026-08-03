542b9e7e12da0dea0db794cad0b135d6d157f0f8
zkr hz <zakariaxhizzi@gmail.com>
2026-08-03 11:23:49 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,11 +6,11 @@ import auth from "../auth/middleware.js";
 
 const router = express.Router();
 
-const isProd = process.env.NODE_ENV === "production";
+
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: isProd,
-  sameSite: isProd ? "none" : "lax",
+  secure: true,
+  sameSite:"none" ,
   maxAge: 7 * 24 * 60 * 60 * 1000,
   path: "/",
 };
